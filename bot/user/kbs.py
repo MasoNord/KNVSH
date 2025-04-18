863171545365534f8d69375b5e1596bf43ffb9e5
MasoNord <trotf233@gmail.com>
2025-04-19 00:59:59 +0300

    Implement get events button

--- bot/user/kbs.py
+++ bot/user/kbs.py
@@ -11,3 +11,35 @@ def main_user_kb(user_id: int) -> InlineKeyboardMarkup:
         kb.button(text="⚙️ Админ панель", callback_data="admin_panel")
     kb.adjust(1)
     return kb.as_markup()
+
+def get_events_kb(events, start) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    # keyboards = []
+    # temp = []
+
+    # for i in range(len(events)):
+    #     if len(temp) == 5:
+    #         keyboards.append(temp)
+    #         temp = []
+    #     else:
+    #         temp.append(events[i])
+        
+
+    # for i in range(keyboards[start]):
+    #     adjust_elements.append(1)
+    #     kb.button(text=keyboards[start][i].title, callback_data=f"get_event_by_{i}")
+
+    adjust_elements = []
+    for i in range(len(events)):
+        adjust_elements.append(1)
+        kb.button(text=f"{events[i].title}", callback_data=f"get_event_by_{i}")
+
+    adjust_elements.append(3)
+    kb.button(text="⬅️", callback_data="back_to_list")
+    kb.button(text=f"{start}/{len(events)}", callback_data="reminder")
+    kb.button(text="➡️", callback_data="forward_to_list")
+    adjust_elements.append(1)
+    kb.button(text="🏠 На главную", callback_data="home")
+
+    kb.adjust(*adjust_elements,3)
+    return kb.as_markup()
--- bot/user/user.py
+++ bot/user/user.py
@@ -1,10 +1,11 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from sqlalchemy.ext.asyncio import AsyncSession
-from bot.dao.dao import UserDAO
+from bot.admin.utils import process_del_text_message
+from bot.dao.dao import UserDAO, EventDAO
 from bot.user.schemas import TelegramIDModel, UserModel
-from bot.user.kbs import main_user_kb
+from bot.user.kbs import main_user_kb, get_events_kb
 
 user_router = Router()
 
@@ -12,7 +13,7 @@ user_router = Router()
 @user_router.message(CommandStart())
 async def cmd_start(message: Message, session_with_commit: AsyncSession):
     user_id = message.from_user.id
-
+    message.delete()
     user_info = await UserDAO.find_one_or_none(
         session = session_with_commit,
         filters = TelegramIDModel(telegram_id=user_id)
@@ -34,3 +35,22 @@ async def cmd_start(message: Message, session_with_commit: AsyncSession):
     await UserDAO.add(session = session_with_commit, values=values)
     await message.answer(f"🎆🎇🎈🎉 <b>Благодарим за регистрацию!</b> Теперь выберите необходимое действие.",
                          reply_markup=main_user_kb(user_id))
+    
+@user_router.callback_query(F.data == "home")
+async def home(call: CallbackQuery):
+    user_id = call.from_user.id
+    await call.message.delete()
+    await call.message.answer (
+        text="Выберите необходимое действие",
+        reply_markup=main_user_kb(user_id)
+    )
+
+@user_router.callback_query(F.data == "get_events")
+async def get_events(call: CallbackQuery, session_with_commit: AsyncSession):
+    await call.message.delete()
+    events = await EventDAO.find_all(session=session_with_commit, filters=None)
+
+    await call.message.answer(
+        text = "Список доступных мероприятий",
+        reply_markup=get_events_kb(events=events, start=0)
+    )
